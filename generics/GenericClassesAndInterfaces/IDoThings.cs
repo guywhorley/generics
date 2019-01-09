b0e1cd51028bca5533147adde50d51d3d7d6cba3
Guy Whorley <guy.whorley@cherwell.com>
2019-01-08 20:19:11 -0800

    Generic Interfaces

---
+++ generics/GenericClassesAndInterfaces/IDoThings.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassesAndInterfaces
+{
+
+    // this would be the base interface
+    interface IDoThings<T> // : IEnumerable<T>
+    {
+        bool IsValid { get; }
+
+        void DoIt(T value);
+
+        bool DoItAndLetMeKnow(T value);
+    }
+
+    // this class could be one of many repositories
+    public class DoOtherThings<T> : IDoThings<T>
+    {
+        public bool IsValid { get; }
+        public void DoIt(T value)
+        {
+            Console.WriteLine($"Doing OTHER things now... [{value}]");
+        }
+
+        public bool DoItAndLetMeKnow(T value)
+        {
+            Console.WriteLine($"Doing OTHER things now and returning true [{value}]");
+            return true;
+        }
+    }
+
+    // another repository
+    public class DoThings<T> : IDoThings<T>
+    {
+        public bool IsValid { get; }
+        public void DoIt(T value)
+        {
+            Console.WriteLine($"Doing things now... [{value}]");
+        }
+
+        public bool DoItAndLetMeKnow(T value)
+        {
+            Console.WriteLine($"Doing things now and returning true [{value}]");
+            return true;
+        }
+    }
+}
---
+++ generics/GenericClassesAndInterfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClassesAndInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDoThings<string> dt = new DoOtherThings<string>();
+            dt.DoIt("do it now...");
+            dt.DoItAndLetMeKnow("do it and return a bool");
+
+            Console.WriteLine("Press the enter key");
+            Console.ReadLine();
+        }
+    }
+}
